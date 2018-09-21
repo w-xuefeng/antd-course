92e2497035a885af23bccb7238fec47e7169ce55
wangxuefeng <wangxuefeng@corp.netease.com>
2018-09-21 17:08:23 +0800

    feat: add layout and outher pages

--- config/config.js
+++ config/config.js
@@ -1,5 +1,4 @@
 export default {
-    singular: true,
     plugins: [
         ['umi-plugin-react', {
             antd: true
@@ -7,6 +6,25 @@ export default {
     ],
     routes: [{
         path: '/',
-        component: './HelloWorld',
+        component: '../layout',
+        routes: [
+            {
+                path: '/',
+                component: 'Helloworld',
+            },
+            {
+                path: '/helloworld',
+                component: 'Helloworld'
+            },
+            {
+                path: '/dashboard',
+                routes: [
+                    { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+                    { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+                    { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+                ]
+            },
+        ]
     }],
+    singular: true,
 };
